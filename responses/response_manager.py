291b75d4b52a237159c6f163486a168fe098a89c
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-15 19:56:52 -0400

    evaluator spits out current score

--- responses/response_manager.py
+++ responses/response_manager.py
@@ -1,10 +1,12 @@
 class ResponseManager:
     RESPONSES = {
-        range(9, 11): "GREAT DAY FOR GOLF",
+
     }
 
+
     def get_response(self, score):
-        for score_range, response in self.RESPONSES.items():
-            if score in score_range:
-                return response
-        return "couldn't determine the weather condition"
+        print(score)
+        # for score_range, response in self.RESPONSES.items():
+        #     if score in score_range:
+        #         return response
+        # return "couldn't determine the weather condition"
