de4e9bafa741a5735fc532f2e92b0d6b83eb50bd
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-15 19:42:40 -0400

    chore: removed unneeded comment

--- models/checker.py
+++ models/checker.py
@@ -10,25 +10,3 @@ class WeatherChecker:
     def can_golf(self):
         combined_score = self.weather.evaluate_conditions()
         return self.response_manager.get_response(combined_score)
-
-
-# def parse_today_weather(data):
-#     current_weather = data["current"]
-#
-#     weather = {
-#         "temp": current_weather["temp_f"],
-#         "feels_like": current_weather["feelslike_f"],
-#         "humidity": current_weather["humidity"],
-#         "uv_index": current_weather["uv"],
-#         "precip": current_weather["precip_in"],
-#         "wind": current_weather["wind_mph"],
-#         "gusts": current_weather["gust_mph"],
-#     }
-#
-#     return weather
-#
-#
-# def parse_location(data):
-#     location = data["location"]
-#
-#     return location
