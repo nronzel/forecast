56999c6a60513ce7c404e9deddb73d8c2c21edb7
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-15 15:39:41 -0400

    refactor: separated fetch and parse functions

--- api/weather.py
+++ api/weather.py
@@ -23,25 +23,3 @@ def fetch_today_weather(location="auto:ip"):
         return data
     else:
         print(f"Failed with status code: {response.status_code}")
-
-
-def parse_today_weather(data):
-    current_weather = data["current"]
-
-    weather = {
-        "temp": current_weather["temp_f"],
-        "feels_like": current_weather["feelslike_f"],
-        "humidity": current_weather["humidity"],
-        "uv_index": current_weather["uv"],
-        "precip": current_weather["precip_in"],
-        "wind": current_weather["wind_mph"],
-        "gusts": current_weather["gust_mph"],
-    }
-
-    return weather
-
-
-def parse_location(data):
-    location = data["location"]
-
-    return location
