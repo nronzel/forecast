c22b4fd9e2a37aa279099a60ef503cab19a941cb
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-15 19:41:44 -0400

    refactor: decoupled evaluators
    evaluators are now instantiated on intitilization
    
    default evaluators are provided and usually just fine, unless you want
    less data towards the total score for variations in the future.

--- models/weather.py
+++ models/weather.py
@@ -1,22 +1,43 @@
+from conditions import evaluator
 from conditions.evaluator import *
 
 
 class Weather:
-    def __init__(self, weather_data):
-        self.weather = self.__clean_data(weather_data)
-        self.temp_evaluator = TempEvaluator(self.weather["temp"])
-        # ...other evaluators (humidity, precip, etc.)
+    def __init__(self, weather_data, evaluators=None):
+        self.weather = self._clean_data(weather_data)
+
+        if evaluators is None:
+            evaluators = {
+                "temp": TempEvaluator,
+                "feels_like": FeelsLikeEvaluator,
+                "humidity": HumidityEvaluator,
+                "uv": UvEvaluator,
+                "wind": WindEvaluator,
+                "gust": GustEvaluator,
+            }
+
+        self.evaluator_instances = {}
+        for key, Evaluator in evaluators.items():
+            if key in self.weather:
+                self.evaluator_instances[key] = Evaluator(self.weather[key])
 
     def evaluate_conditions(self):
-        temp_score = self.temp_evaluator.evaluate()
+        total_score = sum(
+            evaluator.evaluate() for evaluator in self.evaluator_instances.values()
+        )
+        average_score = total_score / len(self.evaluator_instances)
+        return average_score
 
-        # combined_score here
-
-        # TODO: return combined score
-        return temp_score
-
-    def __clean_data(self, weather_data):
+    def _clean_data(self, weather_data):
         c = weather_data["current"]
-        clean_weather = {"temp": c["temp_f"]}
+        clean_weather = {
+            "temp": c["temp_f"],
+            "feels_like": c["feelslike_f"],
+            "humidity": c["humidity"],
+            "uv": c["uv"],
+            "wind": c["wind_mph"],
+            "gust": c["gust_mph"],
+            # "precip": c["precip_in"],
+        }
 
         return clean_weather
