6fe65f15da384100ef9bb58b7ba2ad2f76c0d4f5
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-15 17:51:39 -0400

    feat: cleaned weather data

--- models/weather.py
+++ models/weather.py
@@ -3,7 +3,8 @@ from conditions.evaluator import *
 
 class Weather:
     def __init__(self, weather_data):
-        self.temp_evaluator = TempEvaluator(weather_data["temp"])
+        self.weather = self.__clean_data(weather_data)
+        self.temp_evaluator = TempEvaluator(self.weather["temp"])
         # ...other evaluators (humidity, precip, etc.)
 
     def evaluate_conditions(self):
@@ -11,4 +12,11 @@ class Weather:
 
         # combined_score here
 
+        # TODO: return combined score
         return temp_score
+
+    def __clean_data(self, weather_data):
+        c = weather_data["current"]
+        clean_weather = {"temp": c["temp_f"]}
+
+        return clean_weather
