0dcdf87ba39ab3c39be092fe3309ce36e8194033
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-15 16:51:16 -0400

    refactor: created response manager
    continue of my refactor to use composition

---
+++ responses/response_manager.py
@@ -0,0 +1,10 @@
+class ResponseManager:
+    RESPONSES = {
+        range(9, 11): "GREAT DAY FOR GOLF",
+    }
+
+    def get_responses(self, score):
+        for score_range, response in self.RESPONSES.items():
+            if score in score_range:
+                return response
+        return "couldn't determine the weather condition"
