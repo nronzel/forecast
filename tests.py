7213f32c69856bd49c169bd667b9ff131b4356db
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-16 15:15:07 -0400

    refactor: generic evaluator testing
    made a generic test for evaluators to avoid having to test each one
    individually.

--- tests.py
+++ tests.py
@@ -2,16 +2,39 @@ import unittest
 import random
 
 from responses.response_manager import ResponseManager
+from conditions.evaluator import Evaluator, FeelsLikeEvaluator, TempEvaluator
+
+EVALUATORS = [
+    {"class": TempEvaluator, "param": "temp"},
+    {"class": FeelsLikeEvaluator, "param": "feels_like"},
+]
 
 
-def generate_floats(n):
-    return [round(random.uniform(0, 10), 2) for _ in range(n)]
+class TestEvaluators(unittest.TestCase):
+    def evaluator_test(self, evaluator_class):
+        values = generate_floats(150, 10000)
+
+        for value in values:
+            # create evaluator instance with random value
+            eval = evaluator_class(value)
+            score = eval.evaluate()
+
+            # check if score is in correct range
+            expected = 1
+            for (min, max), s in eval.scoring_ranges:
+                if min <= value <= max:
+                    expected = s
+                    break
+
+            self.assertEqual(
+                score, expected, f"curr {value}, expected {expected}, got {score}"
+            )
 
 
 class Tests(unittest.TestCase):
     def test_response_manager_responses(self):
         rm = ResponseManager()
-        test_scores = generate_floats(100)
+        test_scores = generate_floats(10, 10000)
 
         for score in test_scores:
             response = rm.get_response(score)
@@ -31,6 +54,41 @@ class Tests(unittest.TestCase):
                         f"curr score {score}, expected {expected_response} but got {response}",
                     )
                     break
+
+    def test_find_score(self):
+        list = [
+            ((45, 50.99), 2),
+            ((51, 60.99), 5),
+            ((61, 65.99), 6),
+            ((66, 70.99), 8),
+            ((71, 79.99), 10),
+            ((80, 83.99), 9),
+            ((84, 86.99), 6),
+            ((87, 93.99), 3),
+        ]
+
+        eval = Evaluator()
+        values = generate_floats(150, 10000)
+        for value in values:
+            score = eval.find_score(list, value)
+            expected = 1
+            for (min, max), s in list:
+                if min <= value <= max:
+                    expected = s
+                    break
+                expected = 1
+            self.assertEqual(
+                score, expected, f"curr {value}, expected {expected}, got {score}"
+            )
+
+
+for evaluator_info in EVALUATORS:
+    test_func = lambda self, e=evaluator_info: self.evaluator_test(e["class"])
+    setattr(TestEvaluators, f"test_{evaluator_info['class'].__name__}", test_func)
+
+
+def generate_floats(max_num, num_amount):
+    return [round(random.uniform(0, max_num), 2) for _ in range(num_amount)]
 
 
 if __name__ == "__main__":
