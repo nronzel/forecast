5a9c26bfba6c883b10d3f80051ccf269d698cc8b
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-16 12:18:52 -0400

    refactor: fixed numbers - tests pass

--- responses/response_manager.py
+++ responses/response_manager.py
@@ -1,17 +1,14 @@
 class ResponseManager:
     RESPONSES = {
-        (1, 1): "Don't go outside",
-        (2, 3): "2 or 3",
-        (4, 5): "4 or 5",
-        (6, 7): "6 or 7",
-        (8, 9): "8 or 9",
-        (10, 10): "Perfect day!",
+        (0, 1.99): "Don't go outside",
+        (2, 3.99): "2 or 3",
+        (4, 5.99): "4 or 5",
+        (6, 7.99): "6 or 7",
+        (8, 9.99): "8 or 9",
+        (10, 10.99): "Perfect day!",
     }
 
     def get_response(self, score):
-        formatted_score = format(score, ".2f")
-        print(formatted_score)
-
         for (low, high), response in self.RESPONSES.items():
             if low <= score <= high:
                 return response
