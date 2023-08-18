57610ea58f094588178246ef0ea663604b1ecaf7
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-18 16:22:33 -0400

    feat: slightly better responses..

--- responses/response_manager.py
+++ responses/response_manager.py
@@ -1,12 +1,11 @@
 class ResponseManager:
-    # gpt generated responses for now..
     RESPONSES = {
-        (0, 1.99): "Golf today? Might as well play Russian roulette.",
+        (0, 1.99): "Not today, don't even get out of bed.",
         (2, 3.99): "Brave the course today, and your ball isn't the only thing that'll be lost.",
-        (4, 5.99): "Golfing today is like playing on hard mode. Good luck.",
-        (6, 7.99): "The course might just play you today.",
-        (8, 9.99): "Decent conditions, but don't be surprised if the wind betrays you.",
-        (10, 10.99): "Nature's indifferent today. Your swing? That's on you.",
+        (4, 5.99): "Expect some challenges if you golf today..",
+        (6, 7.99): "Not the best weather, but golfable.",
+        (8, 9.99): "Almost perfect weather for golf, your swing? That's on you..",
+        (10, 10.99): "Better call out sick and get the next tee time, weather is perfect!",
     }
 
     def get_response(self, score):
