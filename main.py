59d64e491206eeaca03592b7afffa12b6b8898d4
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-19 20:41:29 -0400

    chore: updated prompt

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from api.weather_fetcher import WeatherFetcher
 from models.checker import WeatherChecker
 
 if __name__ == "__main__":
-    location = input("Enter your location (or leave empty for auto:ip): ")
+    location = input("Enter your location (or leave empty for auto): ")
     if location == "":
         location = "auto:ip"
+
     fetcher = WeatherFetcher(location)
     weather_data = fetcher.fetch_weather()
 
