9222f0fe38ae3c15c81df76cada5daa91b2d54fd
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-20 17:03:36 -0400

    refactor: rewrote to utilize composition

--- api/parser.py
+++ api/parser.py
@@ -1,48 +1,111 @@
 class Parser:
+    def __init__(self, raw_data, hour=None):
+        self.raw_data = raw_data
+        self.location_parser = LocationParser(self.raw_data)
+        self.parsed_location_data = self.location_parser.parse()
+
+        self.hour = hour
+        if hour is None:
+            self.hour = self._get_current_hour()
+
+        self.weather_parser = WeatherParser(self.raw_data, self.hour)
+        self.parsed_weather_data = self.weather_parser.parse()
+
+    def _get_current_hour(self):
+        try:
+            time = self.parsed_location_data["localtime"]
+            return int(time.split(" ")[1].split(":")[0])
+        except KeyError as e:
+            print(f"Missing key in raw location data: {e}")
+
+
+class LocationParser:
     def __init__(self, raw_data):
-        self.parsed_weather_data = self._parse_weather_data(raw_data)
-        self.parsed_location_data = self._parse_location_data(raw_data)
+        self.raw_data = raw_data
 
-    def _parse_weather_data(self, raw_data):
+    def parse(self):
+        try:
+            location_data = self.raw_data["location"]
+            return {
+                "city": location_data["name"],
+                "state": location_data["region"],
+                "localtime": location_data["localtime"],
+            }
+        except KeyError as e:
+            print(f"Missing key in raw location data: {e}")
+            return {}
+
+
+class WeatherParser:
+    def __init__(self, raw_data, hour=None):
+        self.raw_data = raw_data
+        self.hour = hour
+
+    def parse(self):
+        current_weather_parser = CurrentWeatherParser(self.raw_data)
+        todays_forecast_parser = TodaysForecastParser(self.raw_data)
+        hourly_weather_parser = HourlyWeatherParser(self.raw_data, self.hour)
+
         return {
-            "todays_forecast": self._parse_todays_forecast(
-                raw_data["forecast"]["forecastday"][0]["day"]
-            ),
-            "current_weather": self._parse_current_weather(raw_data["current"]),
-            "hourly_weather": self._parse_hourly_weather(
-                raw_data["forecast"]["forecastday"][0]["hour"]
-            ),
+            "current_weather": current_weather_parser.parse(),
+            "todays_forecast": todays_forecast_parser.parse(),
+            "hourly_weather": hourly_weather_parser.parse(),
         }
 
-    def _parse_location_data(self, raw_data):
-        location_data = raw_data["location"]
-        parsed_data = {
-            "city": location_data["name"],
-            "state": location_data["region"],
-            "localtime": location_data["localtime"],
-        }
-        return parsed_data
 
-    def _parse_current_weather(self, current_weather_data):
+class CurrentWeatherParser:
+    def __init__(self, raw_data):
+        self.raw_data = raw_data["current"]
+
+    def parse(self):
+        try:
+            return {
+                "temp": self.raw_data["temp_f"],
+                "feels_like": self.raw_data["feelslike_f"],
+                "humidity": self.raw_data["humidity"],
+                "uv": self.raw_data["uv"],
+                "wind": self.raw_data["wind_mph"],
+                "gust": self.raw_data["gust_mph"],
+                "condition": self.raw_data["condition"]["text"],
+            }
+
+        except KeyError as e:
+            print(f"Missing key in raw current weather data: {e}")
+            return {}
+
+
+class HourlyWeatherParser:
+    def __init__(self, raw_data, hour):
+        self.raw_data = raw_data["forecast"]["forecastday"][0]["hour"]
+        self.hour = hour
+
+    def parse(self):
+        if self.hour is not None:
+            self.hour = int(self.hour)
+
+        data = self.raw_data[self.hour]
         return {
-            "temp": current_weather_data["temp_f"],
-            "wind": current_weather_data["wind_mph"],
-            "gust": current_weather_data["gust_mph"],
-            "humidity": current_weather_data["humidity"],
-            "feels_like": current_weather_data["feelslike_f"],
-            "uv": current_weather_data["uv"],
-            "current_condition": current_weather_data["condition"]["text"],
+            "temp": data["temp_f"],
+            "feels_like": data["feelslike_f"],
+            "humidity": data["humidity"],
+            "uv": data["uv"],
+            "wind": data["wind_mph"],
+            "gust": data["gust_mph"],
+            "rain_chance": data["chance_of_rain"],
+            "snow_chance": data["chance_of_snow"],
         }
 
-    def _parse_todays_forecast(self, todays_forecast_data):
+
+class TodaysForecastParser:
+    def __init__(self, raw_data):
+        self.raw_data = raw_data["forecast"]["forecastday"][0]["day"]
+
+    def parse(self):
         return {
-            "avg_temp": todays_forecast_data["avgtemp_f"],
-            "max_wind": todays_forecast_data["maxwind_mph"],
-            "avg_humidity": todays_forecast_data["avghumidity"],
-            "rain_chance": todays_forecast_data["daily_chance_of_rain"],
-            "snow_chance": todays_forecast_data["daily_chance_of_snow"],
-            "todays_condition": todays_forecast_data["condition"]["text"],
+            "avg_temp": self.raw_data["avgtemp_f"],
+            "max_wind": self.raw_data["maxwind_mph"],
+            "avg_humidity": self.raw_data["avghumidity"],
+            "rain_chance": self.raw_data["daily_chance_of_rain"],
+            "snow_chance": self.raw_data["daily_chance_of_snow"],
+            "condition": self.raw_data["condition"]["text"],
         }
-
-    def _parse_hourly_weather(self, hourly_weather_data):
-        print(hourly_weather_data)
