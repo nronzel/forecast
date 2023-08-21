9f9eca0a0f495b6770174c583c452a4e948504d6
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-21 10:57:25 -0400

    updated message

--- responses/response_manager.py
+++ responses/response_manager.py
@@ -4,7 +4,7 @@ class ResponseManager:
         (2, 3.99): "Brave the course today, and your ball isn't the only thing that'll be lost.",
         (4, 5.99): "Expect some challenges if you golf today..",
         (6, 7.99): "Not the best weather, but golfable.",
-        (8, 9.99): "Almost perfect weather for golf, your swing? That's on you..",
+        (8, 9.99): "Almost perfect weather for golf. Your swing? That's on you..",
         (10, 10.99): "Better call out sick and get the next tee time, weather is perfect!",
     }
 
