2aa0b668eca4048a62fe7e8f4149a70699ef1178
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-22 09:17:45 -0400

    chore: added type hints

--- models/input_verifier.py
+++ models/input_verifier.py
@@ -2,15 +2,15 @@ import re
 
 
 class Verifier:
-    def verify(self):
+    def verify(self) -> bool:
         raise NotImplementedError
 
 
 class LocationVerifier(Verifier):
-    def __init__(self, location):
+    def __init__(self, location: str):
         self.location = location
 
-    def verify(self):
+    def verify(self) -> bool:
         if self.is_zip_code(self.location):
             return True
         elif self.is_city_state(self.location):
@@ -23,25 +23,25 @@ class LocationVerifier(Verifier):
             return False
 
     @staticmethod
-    def is_zip_code(location):
+    def is_zip_code(location: str) -> bool:
         return bool(re.fullmatch(r"\d{5}", location))
 
     @staticmethod
-    def is_city_state(location):
+    def is_city_state(location: str) -> bool:
         return bool(re.fullmatch(r"[a-zA-Z\s]+[,\s]\s*[a-zA-Z\s]+", location))
 
     @staticmethod
-    def is_city(location):
+    def is_city(location: str) -> bool:
         return bool(re.fullmatch(r"[a-zA-Z\s]+", location))
 
     @staticmethod
-    def is_default(location):
+    def is_default(location: str) -> bool:
         return location == "auto:ip"
 
 
 class ApiKeyVerifier(Verifier):
-    def __init__(self, api_key):
+    def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def verify(self):
+    def verify(self) -> bool:
         return bool(re.fullmatch(r"[a-zA-Z0-9]{32}", self.api_key))
